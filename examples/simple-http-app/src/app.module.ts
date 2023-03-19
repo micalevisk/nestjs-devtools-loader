ad6c55f0e5c05d1259faa405f937f9ce00059711
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2023-03-18 23:56:33 -0400

    fix: deal with create invoked with no options

--- examples/simple-http-app/src/app.module.ts
+++ examples/simple-http-app/src/app.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
-@Module({})
+@Module({
+  controllers: [AppController],
+  providers: [AppService],
+})
 export class AppModule {}
