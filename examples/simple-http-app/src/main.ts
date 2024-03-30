2b23c2ec4e4ed1f7374d0517611b4b4048d3cf7b
Micael Levi L. Cavalcante <micalevisk@gmail.com>
2024-03-30 18:23:09 -0400

    test: add all cases that the lib should act in

--- examples/simple-http-app/src/main.ts
+++ examples/simple-http-app/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import type { NestExpressApplication } from '@nestjs/platform-express';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // Use case 1: default adapter, default options
+  const app = await NestFactory.create(AppModule);
+
+  // Use case 2: custom adapter, default options
+  // const app = await NestFactory.create(AppModule, new FastifyAdapter());
+
+  // Use case 3: default adatper, custom options
+  // const app = await NestFactory.create(AppModule, { logger: ['debug'] });
+
+  // Use case 4: custom adatper, custom options
+  // const app = await NestFactory.create(AppModule, new FastifyAdapter(), { logger: ['debug'] });
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
